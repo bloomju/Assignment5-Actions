029dd1af0a71e269ea01eb1849ed901d35e88cae
Justin Bloom <bloomju@oregonstate.edu>
2020-03-03 13:13:21 -0800

    MY COMMENT HAS AN ERROR???

--- task.py
+++ task.py
@@ -18,7 +18,7 @@ def numDays(date1, date2):
     return abs((date2-date1).days)
 
 
-#this function has one purpose: to use datetime.date. The program runs just fine without it, but not on git.
+# this function has one purpose: to use datetime.date. The program runs just fine without it, but not on git.
 def getridofwarning():
     d = date(1, 1, 1)
     d = d-d
